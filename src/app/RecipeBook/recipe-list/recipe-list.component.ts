21013948aaee60bfe4f231bb5b17e70a914ef6ec
Steve Stranczek <stevestranczek@gmail.com>
2020-10-19 15:27:30 -0600

    Created recipe model and worked on list component

--- src/app/RecipeBook/recipe-list/recipe-list.component.ts
+++ src/app/RecipeBook/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe', 'This is simply a test','https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.P0OEF9nbU7wL9nOxqE3azgHaEK%26pid%3DApi&f=1' )
+  
+  ];
   constructor() { }
 
   ngOnInit(): void {
