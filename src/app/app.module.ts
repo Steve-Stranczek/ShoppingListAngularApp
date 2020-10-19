21133e68d7251e6982f3de9fe15f4aff2644aa5b
Steve Stranczek <stevestranczek@gmail.com>
2020-10-19 13:18:45 -0600

    Added base level components for this app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './ShoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './ShoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './RecipeBook/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './RecipeBook/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
